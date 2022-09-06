e42cefdf88d4be37bbf91bda85ea59fe2c3ed860
Elena Shu Xian Foong <73242679+elenasxfoong@users.noreply.github.com>
2022-09-06 13:38:06 +0800

    Update AS82 STRING REVERSAL FUNCTION.py

--- AS82 STRING REVERSAL FUNCTION.py
+++ AS82 STRING REVERSAL FUNCTION.py
@@ -4,7 +4,7 @@ string = input("Enter a string: ")
 
 def string_reverse (string):
   length = len(string)
-  reversed = string[length::-1]
+  reversed = string[length::-1] #starts at string length: ends index 0: in steps of 1 in reverse order (negative index means reversed) #[::-1] is an alternative
   print (reversed)
   return reversed
     
